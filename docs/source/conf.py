69ebdf33f1e6c304855f8db36d52ddb3c2cbaa02
Zain Ali <zainbalouch3@gmail.com>
2021-06-14 02:43:22 +1000

    rem build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PyRapidML'
-copyright = '2021, Zain Ali'
-author = 'Zain Ali'
+project = "PyRapidML"
+copyright = "2021, Zain Ali"
+author = "Zain Ali"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.13'
+release = "1.0.13"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +31,27 @@ release = '1.0.13'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
 
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+autodoc_mock_imports = ["setup"]
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+# Sort methods by the order they are found in the source files
+autodoc_member_order = "bysource"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +59,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+html_css_files = ["css/custom.css"]
+
+master_doc = "index"
